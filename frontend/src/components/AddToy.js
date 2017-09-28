7bab9d6db6fe8567d5b1324799449cb01632a527
Noe Tellez <noetlz10@gmail.com>
2017-09-27 19:08:35 -0500

    adding components

--- frontend/src/components/AddToy.js
+++ frontend/src/components/AddToy.js
@@ -1,10 +1,39 @@
 import React, {Component} from 'react';
+import ToyService from './components/ToyService';
 
 class AddToy extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {toy: ''};
+        //this sends the new toy via axios to the server
+        this.addToyService = new ToyService();
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({toy: event.target.value});
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        this.addToyService.sendData(this.state.toy);
+        this.props.history.push('/index'); 
+    }
+
+   
     render() {
         return (
-            <div>
+            <div className = "container">
                 <h2> Welcome to Add a Toy </h2>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Add Toy:
+                        <input type="text" value={this.state.toy}
+                        onChange={this.handleChange}
+                        className="form-control" />
+                    </label>    
+                </form>
             </div>    
         );
     }
---
+++ frontend/src/components/ToyService.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+class ToyService {
+    sendDate(data) {
+        axios.post('http://localhost:4200/toys/add/post', {
+            toy: data
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+    }
+}
+
+export default ToyService;
